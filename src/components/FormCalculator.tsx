6db38ff591efce89be49184d148ee2d8437a7cb5
Alan <pinhoneibor32@gmail.com>
2023-08-24 15:52:08 -0600

    chore: future day validation in FormCalculator

--- src/components/FormCalculator.tsx
+++ src/components/FormCalculator.tsx
@@ -1,37 +1,53 @@
 import { useState } from "react";
 import { FormInput } from "./FormInput";
-import { calculateAge } from "../helpers/calculateAge";
+import { calculateAge, validateFutureDate } from "../helpers/index";
 import { TDate } from "../types/TDate";
 
 export const FormCalculator = () => {
   
-  const [date, setDate] = useState<TDate | null>(null);
-  const [errors, setErrors] = useState({day:'', month:'', year:''})
-  
-  const handleCalculateAge = () => {  
-    calculateAge(date!);
-  };
+  const [date, setDate] = useState<TDate>({ day: '', month: '', year: ''});
+  const [errors, setErrors] = useState({day:'', month:'', year:''});
 
   const handleDateValue = (property: keyof TDate, value:string) => {
 
-    if(property === 'day') {
-      // Matches numbers 01-31, leading 0 is optional
-      const isValidDay = /^(0?[1-9]|[12][0-9]|3[01])$/.test(value);
-      setErrors( prevError => ( {...prevError, day: !isValidDay ? 'Must be a valid day' : ''} ) );
-    } 
-    if(property === 'month') {
-      // Matches numbers 01-12, leading 0 is optional
-      const isValidMonth = /^(0?[1-9]|1[0-2])$/.test(value);
-      setErrors( prevError => ( {...prevError, month: !isValidMonth ? 'Must be a valid month' : ''} ) );
-    } 
-    if(property === 'year') {
-      // Matches 1 to 4 digits for year
-      const isValidYear = /^\d{1,4}$/.test(value);
-      setErrors( prevError => ( {...prevError, year: !isValidYear ? 'Must be a valid year' : ''} ) );
-    } 
+    const validateDate = (dateValue:string, regEx:RegExp, msgError:string) => {
+      const isValidDate = regEx.test(dateValue);
+      
+      if(dateValue === '') {
+        setErrors( prevError => ( {...prevError, [property]: 'This field is required'} ) );
+        return false;
+      }
+      if(!isValidDate) {
+        setErrors( prevError => ( {...prevError, [property]: msgError} ) );
+        return false;
+      }
+      setErrors( prevError => ( {...prevError, [property]: ''} ) );
+      return true;
+    }
 
-    setDate( oldDate => ( { ...oldDate, [property]: value } as TDate) ); 
+    if(property === 'day') validateDate(value, /^(0?[1-9]|[12][0-9]|3[01])$/, 'Must be a valid day'); // RegEx matches numbers 01-31, leading 0 is optional
+
+    if(property === 'month') validateDate(value, /^(0?[1-9]|1[0-2])$/, 'Must be a valid month'); // RegEx matches numbers 01-12, leading 0 is optional
+       
+    if(property === 'year') validateDate(value, /^\d{1,4}$/, 'Must be a valid year'); //RegEx matches 1 to 4 digits for year
+
+    setDate( oldDate => ( { ...oldDate, [property]: value } as TDate) );
   };
+
+  const handleCalculateAge = () => {     
+    const futureDateErrors = validateFutureDate(date.day, date.month, date.year);
+
+    if(futureDateErrors){
+      return setErrors({
+        day: futureDateErrors,
+        month:futureDateErrors,
+        year: futureDateErrors
+      })
+    }
+
+    calculateAge(date!);
+    setErrors({day:'', month:'', year:''});
+  }
 
   return (
     <>
--- src/helpers/calculateAge.ts
+++ src/helpers/calculateAge.ts
@@ -2,33 +2,6 @@ import { TDate } from "../types/TDate";
 
 export const calculateAge = ({day, month, year}:TDate) => {
 
-  //Check if dates are in the future
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth() + 1;
-  const currentDay = new Date().getDate();
-
-  const isFutureYear = Number(year) > currentYear;
-  const isFutureMonth = Number(year) === currentYear && Number(month) > currentMonth;
-  const isFutureDay = Number(year) === currentYear && Number(month) === currentMonth && Number(day) > currentDay;
-
-  const has30Days = [4, 6, 9, 11].includes(Number(month)); // Check months with 30 days
-  const has31Days = [1, 3, 5, 7, 8, 10, 12].includes(Number(month)); // Check months with 31 days
-  const isLeapYear = (Number(year) % 4 === 0 && Number(year) % 100 !== 0) || (Number(year) % 400 === 0); // Check if the year is a leap year
-
-  const isDayInvalid = () => Number(day) > 31 || (has30Days && Number(day) > 30) || (has31Days && Number(day) > 31);
-  const isMonthInvalid = () => Number(month) < 1 || Number(month) > 12;
-  const isLeapYearInvalid = () => Number(month) === 2 && Number(day) > 29 && isLeapYear;
-  const isNonLeapYearInvalid = () => Number(month) === 2 && Number(day) > 28 && !isLeapYear;
-
-
-  if (isFutureYear || isFutureMonth || isFutureDay) {
-    return console.log('Must be in the past');
-  }
-
-  if (isDayInvalid() || isMonthInvalid() || isLeapYearInvalid() || isNonLeapYearInvalid()) {
-    return console.log('Must be a valid date')
-  }
-
   // Calculate the difference in age from the current date
 
   const birthDate = new Date(Number(year), Number(month) - 1, Number(day));
