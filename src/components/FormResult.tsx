5637bffe0c120a5b116cb4bc99a07ea9fffcb617
Alan <pinhoneibor32@gmail.com>
2023-09-04 20:17:29 -0600

    chore: FormResult component updated

--- src/components/FormResult.tsx
+++ src/components/FormResult.tsx
@@ -6,9 +6,21 @@ export const FormResult = ({ageResults}:resultsProps) => {
 
   return (
     <>
-      <h2>{ageResults?.years ? ageResults.years : '--'} years</h2>
-      <h2>{ageResults?.months ? ageResults.months : '--'} months</h2>
-      <h2>{ageResults?.days ? ageResults.days : '--'} days</h2>
+      <h2>{
+        ageResults?.years
+          ? ageResults.years
+          : (ageResults?.years === 0 ? '0' : '--')} years
+      </h2>
+      <h2>{
+        ageResults?.months
+          ? ageResults.months
+          : (ageResults?.months === 0 ? '0' : '--')} months
+      </h2>
+      <h2>{
+        ageResults?.days
+          ? ageResults.days
+          : (ageResults?.days === 0 ? '0' : '--')} days
+      </h2>
     </>
   )
 }
